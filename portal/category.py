227b544341d35d22b72d04bb05f0273b8589a592
Андрей <SergeevAndreyAleksandrovich@yandex.ru>
2024-10-18 01:33:07 +0900

    Create models

---
+++ portal/category.py
@@ -0,0 +1,25 @@
+'Танки', 'Хилы', 'ДД', 'Торговцы', 'Гилдмастеры', 'Квестгиверы', 'Кузнецы', 'Кожевники', \
+	'Зельевары', 'Мастера заклинаний'
+TANKS = 'TN'
+HEELS = 'HL'
+DD = 'DD'
+TRADERS = 'TR'
+GILDMASTERS = 'GL'
+QUESTGIVERS = 'QS'
+BLACKSMITHS = 'BL'
+TANNERS = 'TN'
+CREATORSPOTION = 'CR'
+WIZARD = 'WR'
+
+CATEGORY_CHOICE = [
+	(TANKS, "Танки"),
+	(HEELS, "Хилы"),
+	(DD, "ДД"),
+	(TRADERS, "Торговцы"),
+	(GILDMASTERS, "Гилдмастеры"),
+	(QUESTGIVERS, "Квестгиверы"),
+	(BLACKSMITHS, "Кузнецы"),
+	(TANNERS, "Кожевники"),
+	(CREATORSPOTION, "Зельевары"),
+	(WIZARD, "Мастера заклинаний"),
+	]
--- portal/models.py
+++ portal/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from category import *
 
 # Create your models here.
 
 class Author(models.Model):
-	author = models.OneToOneField(User, on_delete=models.D0_NOTHING)
+	authorUser = models.OneToOneField(User, on_delete=models.D0_NOTHING)
 
 class Post(models.Model):
+	author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
 	title = models.CharField(max_length=64)
-	text = models.TextField(help_text='Введите ваш текст здесь')
-	datetime = models.DateTimeField(auto_now_add=True)
+	text = RichTextField()
+	datecreation = models.DateTimeField(auto_now_add=True)
+	category = models.CharField(max_length=2, choices=CATEGORY_CHOICE, default=WIZARD,
+								help_text='Выберите категорию')
+
+	def __str__(self):
+		return self.title
+
+class Page(models.Model):
+	authorpage = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+	text = models.TextField()
+	status = models.BooleanField(default=False)
+	dateCreation = models.DateTimeField(auto_now_add=True)
 
